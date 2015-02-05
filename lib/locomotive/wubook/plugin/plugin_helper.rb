b1cd7303d00fff64400752af7bb971b006648499
Stefan Eilers <stefan.eilers@intelligentmobiles.com>
2015-02-05 14:54:09 +0100

    Used incorrect compare which does not return boolean..

--- lib/locomotive/wubook/plugin/plugin_helper.rb
+++ lib/locomotive/wubook/plugin/plugin_helper.rb
@@ -4,7 +4,7 @@ module PluginHelper
         def fetch_room_id(wired, lcode, room_id)
                 # Start with finding the room-id for the room with a special name
                 rooms = wired.fetch_rooms(lcode)
-                filtered_rooms = rooms.select { |room_hash| room_hash['shortname'].casecmp room_id }
+                filtered_rooms = rooms.select { |room_hash| room_hash['shortname'] == room_id }
                 ::Locomotive.log "**> Filtered rooms #{filtered_rooms} "
                 raise "Unable to find a room with identifier: #{room_id}" if filtered_rooms.length == 0
                 room_identifier = filtered_rooms[0]['id']
